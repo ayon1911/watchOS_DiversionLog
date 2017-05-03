a239567f6cf4164c404f6df5c2ff4d17f3d3b8a9
ayon1911 <khaledrahman-ayon@Thomass-MacBook-Air.local>
2017-05-03 13:22:11 +0200

    first commit

---
+++ Diversion Log WatchKit Extension/DistractionRowController.swift
@@ -0,0 +1,13 @@
+//
+//  DistractionRowController.swift
+//  Diversion Log
+//
+//  Created by Khaled Rahman-Ayon on 02.05.17.
+//  Copyright © 2017 Khaled Rahman-Ayon. All rights reserved.
+//
+
+import WatchKit
+
+class DistractionRowController: NSObject {
+
+}
---
+++ Diversion Log WatchKit Extension/NotificationsInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  NotificationsInterfaceController.swift
+//  Diversion Log
+//
+//  Created by Khaled Rahman-Ayon on 03.05.17.
+//  Copyright © 2017 Khaled Rahman-Ayon. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class NotificationsInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
